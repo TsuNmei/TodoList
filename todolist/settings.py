e6ac883d4f3216b6bd9432bc38576194edb40734
Ubuntu <ubuntu@ip-172-31-45-2.ap-northeast-1.compute.internal>
2020-11-18 02:44:34 +0000

    config: allowed host

--- todolist/settings.py
+++ todolist/settings.py
@@ -17,14 +17,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '70+^emky=r3ml!xbkw&&#a=_spln4lu0f(yzy90ib+rm53t%c1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://todolist.revtel2.com/']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
